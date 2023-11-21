7c2a9cd0c2c586c44a83f4a634451dbcdb7095a1
busolagbadero <94229949+busolagbadero@users.noreply.github.com>
2023-11-21 05:07:24 +0000

    Create bubu.groovy

---
+++ bubu.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+    echo "building App"
+}
+
+def TestApp() {
+    echo "Testing App"
+}
+
+def DeployApp() {
+    echo "Deploying App"
+    echo "working on version {params.bubu} "
+}
+return this
