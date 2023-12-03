b89a229bb2922fba0151be635ae12a532d2134f1
busolagbadero <94229949+busolagbadero@users.noreply.github.com>
2023-12-03 17:40:04 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,15 @@
 def buildJar() {
-    echo 'Done...'
-    
+    echo 'Building jar...'
+    sh 'mvn package'
 }
 
+def buildImage() {
+    echo 'Building Image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                    sh 'docker build -t gbaderobusola/busola:tolani-1.0 .'
+                    sh "echo $PASS | docker login -u $USER --password-stdin"
+                    sh 'docker push gbaderobusola/busola:tolani-1.0'
+                }
+}
 
 return this
